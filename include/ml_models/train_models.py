6f736aff1c7ae4533e2c0ae13c1b3095b77c9b4d
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-08 18:46:55 +0100

    add the machine learning pipeline configuration : models , feature engineering , data validation ,training and monitoring

--- include/ml_models/train_models.py
+++ include/ml_models/train_models.py
@@ -5,4 +5,8 @@ class ModelTrainer:
     def __init__(self, config_path: str = 'usr/local/airflow/include/config/ml_config.yaml' ):
         with open(config_path, 'r') as f:
             self.config = yaml.safe_load(f)
-        pass
+
+
+
+
+
