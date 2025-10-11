62f5f0703faf3f92b2b18ccb8d8d5d7c5e2b1541
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-12 00:15:20 +0100

    preprocess features

--- include/ml_models/train_models.py
+++ include/ml_models/train_models.py
@@ -1,5 +1,7 @@
 import yaml
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 from utils.mlflow_utils import MlflowManager
 from data_validation.validators import DataValidator
@@ -58,6 +60,51 @@ class ModelTrainer:
 
         logger.info(f"Train data split: {len(train_df)}, Validation data split: {len(val_df)}, Test data split: {len(test_df)} ")
         return train_df, val_df, test_df
+
+    def preprocess_features(self,train_df: pd.DataFrame, val_df: pd.DataFrame,
+                        test_df: pd.DataFrame, target_col:str,
+                        exclude_cols: List[str] = ["date"] ):
+        logger.info(f"Preprocessing features")
+        feature_cols= [col for col in train_df.columns if col not in exclude_cols + [target_col]]
+
+        X_train = train_df[feature_cols].copy()
+        X_val = val_df[feature_cols].copy()
+        X_test = test_df[feature_cols].copy()
+
+        y_train=train_df[target_col].values()
+        y_val=val_df[target_col].values()
+        y_test=test_df[target_col].values()
+
+        #encode categorial variables
+        categorial_cols = X_train.select_dtypes(include=["object"]).columns
+        for col in categorial_cols:
+            if col not in self.encoders:
+                self.encoders[col] = LabelEncoder()
+                X_train.loc[:,col] = self.encoders[col].fit_transform(X_train[col].astype(str))
+            else:
+                X_train.loc[:,col] = self.encoders[col].transform(X_train[col].astype(str))
+
+
+
+        X_val.loc[:,col]=self.encoders[col].transform(X_val[col].astype(str))
+        X_test.loc[:,col] = self.encoders[col].transform(X_test[col].astype(str))
+
+
+        #scale numerical features
+        scaler = StandardScaler()
+        X_train_scaled=scaler.fit_transform(X_train)
+        X_val_scaled=scaler.transform(X_val)
+        X_test_scaled=scaler.transform(X_test)
+
+        X_train_scaled=pd.DataFrame(X_train_scaled,columns=feature_cols,index=X_train.index)
+        X_val_scaled=pd.DataFrame(X_val_scaled,columns=feature_cols,index=X_train.index)
+        X_test_scaled=pd.DataFrame(X_test_scaled,columns=feature_cols,index=X_test.index)
+
+        self.scalers["standard"] = scaler
+        self.feature_cols=feature_cols
+
+        return X_train_scaled, X_val_scaled, X_test_scaled , y_train,y_val,y_test
+
 
 
 
